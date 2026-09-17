505d410875eef035ec15836cfa4f12fb041c73dc
bft2249228496 <2249228496@qq.com>
2026-09-17 15:12:40 +0800

    fix(sub-store): use material_ui package import to pass design package test

--- lib/views/sub_store.dart
+++ lib/views/sub_store.dart
@@ -1,6 +1,6 @@
 import 'package:fl_clash/common/common.dart';
 import 'package:fl_clash/widgets/widgets.dart';
-import 'package:flutter/material.dart';
+import 'package:material_ui/material_ui.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class SubStoreView extends ConsumerStatefulWidget {
