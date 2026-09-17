253d196485b1a585e2ab67a17ff2cdcb88e00fd8
bft2249228496 <2249228496@qq.com>
2026-09-17 15:06:16 +0800

    fix(sub-store): remove unused enum import to pass flutter analyze

--- lib/views/sub_store.dart
+++ lib/views/sub_store.dart
@@ -1,5 +1,4 @@
 import 'package:fl_clash/common/common.dart';
-import 'package:fl_clash/enum/enum.dart';
 import 'package:fl_clash/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
